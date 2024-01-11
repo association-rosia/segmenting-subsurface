a2547923d1c1803d5c97885a910876025be102cf
Louis REBERGA <louis.reberga@gmail.com>
2024-01-11 23:06:19 +0100

    add collate_fn

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -19,7 +19,6 @@ class SegSubDataset(Dataset):
         self.items = self.get_items()
 
         self.processor = Mask2FormerImageProcessor(
-            do_resize=True,
             size={
                 'height': args['config']['data']['size']['height'],
                 'width': args['config']['data']['size']['width']
@@ -50,7 +49,7 @@ class SegSubDataset(Dataset):
 
         inputs = {k: v.squeeze() if isinstance(v, torch.Tensor) else v[0] for k, v in inputs.items()}
 
-        return inputs, item
+        return item, inputs
 
     def get_slice(self, item):
         volume = np.load(item['volume'], allow_pickle=True)
@@ -106,6 +105,21 @@ class SegSubDataset(Dataset):
                 raise ValueError(f'Unknown dimension: {dim}')
 
         return items
+
+
+def collate_fn(batch):
+    pixel_values = torch.stack([el[1]['pixel_values'] for el in batch])
+    pixel_mask = torch.stack([el[1]['pixel_mask'] for el in batch])
+    class_labels = [el[1]['class_labels'] for el in batch]
+    mask_labels = [el[1]['mask_labels'] for el in batch]
+    items = [el[0] for el in batch]
+
+    inputs = {'pixel_values': pixel_values,
+              'pixel_mask': pixel_mask,
+              'class_labels': class_labels,
+              'mask_labels': mask_labels}
+
+    return items, inputs
 
 
 def get_volumes(config, set):
@@ -169,17 +183,16 @@ if __name__ == '__main__':
     from torch.utils.data import DataLoader
 
     config = utils.get_config()
-
     # compute_image_mean_std(config)
 
     set = 'train'
     train_volumes = get_volumes(config, set=set)
     args = {'config': config, 'set': set, 'volumes': train_volumes, 'dim': '0,1'}
     train_dataset = SegSubDataset(args)
-    train_dataloader = DataLoader(dataset=train_dataset, batch_size=8, shuffle=False)
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=8, shuffle=False, collate_fn=collate_fn)
 
-    for inputs, label in train_dataloader:
+    for item, inputs in train_dataloader:
+        print(item)
         print('\npixel_values', inputs['pixel_values'][0].shape)
         print('pixel_mask', inputs['pixel_mask'][0].shape)
-        print('label', label.shape)
         break
