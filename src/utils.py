2524d5b87b90419c5930096276af556ac6b79cd5
Louis REBERGA <louis.reberga@gmail.com>
2024-01-31 22:48:23 +0100

    enable_vae_slicing

--- src/utils.py
+++ src/utils.py
@@ -110,6 +110,7 @@ def load_segformer_model(config, run):
     device = get_device()
     lightning = lightning.to(torch.float16)
     lightning = lightning.to(device)
+    lightning.enable_vae_slicing()
 
     return lightning
 
@@ -133,5 +134,6 @@ def load_segment_anything(config, run):
     device = get_device()
     lightning = lightning.to(torch.float16)
     lightning = lightning.to(device)
+    lightning.enable_vae_slicing()
 
     return lightning
