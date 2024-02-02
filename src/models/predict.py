d70921720f23b93b4992ee18226b1e93bc2a8548
Louis REBERGA <louis.reberga@gmail.com>
2024-02-02 18:36:52 +0100

    print(outputs.shape)

--- src/models/predict.py
+++ src/models/predict.py
@@ -50,7 +50,7 @@ def main():
             save_path = get_save_path(item, submission_path)
             inputs = preprocess(inputs)
             outputs = predict_mask2former(m2f_lightning, m2f_processor, inputs)
-            print(outputs)
+            print(outputs.shape)
             break
             outputs = unprocess(outputs)
             save_outputs(outputs, save_path)
