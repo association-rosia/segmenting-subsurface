9191f0872d5b2bee8bb810fd075e2e35f7585749
BaptisteUrgell <baptiste.u@gmail.com>
2024-02-02 23:46:05 +0100

    feat!: from functionnal to object
    Ajout du changement de nom si c'est en train ou en test

--- src/features/segformer_inference.py
+++ src/features/segformer_inference.py
@@ -13,40 +13,30 @@ from multiprocessing import Process
 from src import utils
 
 
-def main(run_id, split):
+def main(run_id):
     config = utils.get_config()
     run = utils.get_run_config(run_id)
-    path_split = os.path.join(
-        config['path']['data']['processed'][split],
-        f"{run.name}-{run.id}",
-    )
-    os.makedirs(path_split, exist_ok=True)
-    
-    if split == 'train':
-        multiprocess_make_mask(config, run, path_split)
-    elif split == 'test':
-        make_mask(
-            config=config,
-            device=torch.cuda.device(0),
-            list_volume=md.get_volumes(config, set=split),
-            run=run,
-            path_split=path_split
+    for split in ['train', 'test']:
+        path_split = os.path.join(
+            config['path']['data']['processed'][split],
+            f"{run.name}-{run.id}",
         )
+        os.makedirs(path_split, exist_ok=True)
+        multiprocess_make_mask(config, run, split)
 
 
-def multiprocess_make_mask(config, run, path_split):
-    list_volume = md.get_volumes(config, set='train'),
+def multiprocess_make_mask(config, run, split):
+    list_volume = md.get_volumes(config, set=split),
     list_volume_split = split_list_volume(list_volume, torch.cuda.device_count())
     
     list_process = [
-        Process(target=make_mask,
-                kwargs={
-                    'config': config,
-                    'device': torch.cuda.device(i),
-                    'list_volume': sub_list_volume,
-                    'run': run,
-                    'path_split': path_split
-        })
+        Process(target=SegformerInference(
+                    config=config,
+                    device=torch.cuda.device(i),
+                    list_volume=sub_list_volume,
+                    run=run,
+                    split=split
+        ))
         for i, sub_list_volume in enumerate(list_volume_split)
     ]
     for p in list_process:
@@ -63,25 +53,60 @@ def split_list_volume(list_volume, nb_split):
     return list_volume_split
 
 
-def make_mask(config, device, list_volume, run, path_split):
-    segformer_model = load_segformer_model(config, run, device)
+class SegformerInference:
+    def __init__(self, config, device, list_volume, run, split) -> None:
+        self.config = config
+        self.device = device
+        self.list_volume = list_volume
+        self.run = run
+        self.split = split
+        self.volume_min = config['data']['min']
+        self.volume_max = config['data']['max']
+        self.contrast_factor = run.config['contrast_factor']
+        self.processor = utils.get_processor(config, run.config)
+ 
+    def __call__(self):
+        model = load_model(self.config, self.run, self.device)
+        
+        with torch.no_grad():
+            for volume_path in tqdm(self.list_volume):
+                volume_name = os.path.basename(volume_path)
+                binary_mask_path = self.get_mask_path(volume_name)
+                if os.path.exists(binary_mask_path):
+                    continue
+                
+                volume = np.load(volume_path, allow_pickle=True)
+                shape = volume.shape
+                volume = self.preprocess(volume)
+                binary_mask = model(volume)
+                binary_mask = self.postprocess(binary_mask, shape)
+                np.save(binary_mask_path, binary_mask, allow_pickle=True)
 
-    volume_min = config['data']['min']
-    volume_max = config['data']['max']
-    contrast_factor = run.config['contrast_factor']
-    processor = utils.get_processor(config, run.config)
+    def get_mask_path(self, volume_name):
+        path = os.path.join(
+            self.config['path']['data']['processed'][self.split],
+            f"{self.run.name}-{self.run.id}",
+        )
+        path = os.path.join(path, volume_name)
+        if self.split == 'train':
+            path.replace('seismic', 'binary_mask')
+        else:
+            path.replace('vol', 'bmask')
+        
+        return path
     
-    def preprocess(volume: np.ndarray):
-        volume = (volume - volume_min) / (volume_max - volume_min)
+    def preprocess(self, volume: np.ndarray):
+        volume = (volume - self.volume_min) / (self.volume_max - self.volume_min)
         volume = np.moveaxis(volume, 1, 2)
         volume = torch.from_numpy(volume).unsqueeze(1)
-        volume = tvF.adjust_contrast(volume, contrast_factor=contrast_factor)
+        volume = tvF.adjust_contrast(volume, contrast_factor=self.contrast_factor)
         volume = torch.repeat_interleave(volume, repeats=3, dim=1)
-        volume = processor(images=volume, return_tensors='pt')
-        volume = volume.to(device=device)
+        volume = self.processor(images=volume, return_tensors='pt')
+        volume = volume.to(device=self.device, dtype=torch.float16)
         
-        return volume.to(dtype=torch.float16)
-    
+        return volume
+
+    @staticmethod
     def postprocess(binary_mask: torch.Tensor, shape):
         binary_mask = torch.moveaxis(binary_mask, 1, 2)
         binary_mask = tF.interpolate(binary_mask.unsqueeze(dim=1), size=shape[1:], mode="bilinear", align_corners=False)
@@ -89,23 +114,9 @@ def make_mask(config, device, list_volume, run, path_split):
         binary_mask = binary_mask.squeeze(dim=1).numpy(force=True)
         
         return binary_mask.astype(np.bool_)
-
-    with torch.no_grad():
-        for volume_path in tqdm(list_volume):
-            volume_name = os.path.basename(volume_path)
-            binary_mask_path = os.path.join(path_split, volume_name)
-            binary_mask_path = binary_mask_path.replace('seismic', 'binary_mask')
-            if os.path.exists(binary_mask_path):
-                continue
-            volume = np.load(volume_path, allow_pickle=True)
-            shape = volume.shape
-            volume = preprocess(volume)
-            binary_mask = segformer_model(volume)
-            binary_mask = postprocess(binary_mask, shape)
-            np.save(binary_mask_path, binary_mask, allow_pickle=True)
             
 
-def load_segformer_model(config, run, device):
+def load_model(config, run, device):
     train_volumes, val_volumes = md.get_training_volumes(config, run.config)
     processor = utils.get_processor(config, run.config)
     model = ml.get_model(run.config)
