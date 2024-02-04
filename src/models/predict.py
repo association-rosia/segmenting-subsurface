a14d345210e06bb108654f68632c60afc7696133
Louis REBERGA <louis.reberga@gmail.com>
2024-02-04 23:45:48 +0100

    try multiprocessing

--- src/models/predict.py
+++ src/models/predict.py
@@ -80,7 +80,7 @@ def split_list_args(args, nb_split):
 
 def create_sam_input_points(m2f_outputs, item, sam_run):
     torch.multiprocessing.set_start_method('spawn', force=True)
-    m2f_outputs.share_memory()
+    m2f_outputs.share_memory_()
     manager = mp.Manager()
     sam_input_points = manager.list()
 
