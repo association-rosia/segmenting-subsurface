544ea1382f3bd06dd112105010a78d23716f6f15
Louis REBERGA <louis.reberga@gmail.com>
2024-02-04 23:57:54 +0100

    try multiprocessing: clone

--- src/models/predict.py
+++ src/models/predict.py
@@ -82,11 +82,12 @@ def create_sam_input_points(m2f_outputs, item, sam_run):
     mp.set_start_method('spawn', force=True)
     manager = mp.Manager()
     sam_input_points = manager.list()
+    m2f_outputs = m2f_outputs.cpu()
 
     volumes = item['volume']
     slices = item['slice']
 
-    m2f_args = [(m2f_outputs[i].cpu(), volumes[i], slices[i].item(), sam_run.config) for i in range(len(m2f_outputs))]
+    m2f_args = [(m2f_outputs[i], volumes[i], slices[i].item(), sam_run.config) for i in range(len(m2f_outputs))]
     list_args_split = split_list_args(m2f_args, nb_split=torch.cuda.device_count())
 
     list_process = [
