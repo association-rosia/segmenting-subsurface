d40db699664105737b4c54cbece9b9406e1f5f00
Louis REBERGA <louis.reberga@gmail.com>
2024-02-05 13:01:39 +0100

    parallelize inference

--- src/models/predict.py
+++ src/models/predict.py
@@ -55,8 +55,8 @@ def main():
             save_path = get_save_path(item, submission_path)
             m2f_inputs = preprocess(inputs)
 
-            # m2f_outputs = predict_mask2former(m2f_lightning, m2f_processor, m2f_inputs)
-            m2f_inputs, m2f_outputs = get_m2f_outputs_example(config, item, m2f_inputs)
+            m2f_outputs = predict_mask2former(m2f_lightning, m2f_processor, m2f_inputs)
+            # m2f_inputs, m2f_outputs = get_m2f_outputs_example(config, item, m2f_inputs)
 
             sam_input_points, sam_input_points_stack_num = create_sam_input_points(m2f_outputs, item, sam_run)
 
