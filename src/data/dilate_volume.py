76cce882fccbc369b7eb00b16ec3545eaceea120
Louis REBERGA <louis.reberga@gmail.com>
2024-02-06 14:08:05 +0100

    dilate also labels

--- src/data/dilate_volume.py
+++ src/data/dilate_volume.py
@@ -11,9 +11,20 @@ def main():
 
     for volume_path in tqdm(train_volumes):
         volume = np.load(volume_path, allow_pickle=True)
-        indexes = [i for i in range(0, len(volume), 30)]
-        volume = volume[indexes]
-        np.save(volume_path, volume, allow_pickle=True)
+        label_path = volume_path.replace('seismic', 'horizon_labels')
+        label = np.load(label_path, allow_pickle=True)
+
+        if volume.shape[0] == 300:  # to divide by 30 the number of slices
+            indexes = [i for i in range(0, len(volume), 30)]
+            volume = volume[indexes]
+            np.save(volume_path, volume, allow_pickle=True)
+            print(f'Resize {volume_path}')
+
+        if label.shape[0] == 300:  # to divide by 30 the number of slices
+            indexes = [i for i in range(0, len(volume), 30)]
+            label = label[indexes]
+            np.save(label_path, label, allow_pickle=True)
+            print(f'Resize {label_path}')
 
 
 if __name__ == '__main__':
