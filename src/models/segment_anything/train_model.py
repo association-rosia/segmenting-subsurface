539ccf21b402f2915996c4cc170dc0b440a4f2d6
Louis REBERGA <louis.reberga@gmail.com>
2024-02-08 21:58:34 +0100

    fix: refoamt all codes

--- src/models/segment_anything/train_model.py
+++ src/models/segment_anything/train_model.py
@@ -10,6 +10,7 @@ import pytorch_lightning as pl
 
 import make_lightning as ml
 import src.data.make_dataset as md
+import src.models.segment_anything.make_lightning as segment_anything_ml
 from src import utils
 
 import wandb
@@ -21,7 +22,7 @@ def main():
     config = utils.get_config()
     wandb_config = utils.init_wandb('segment_anything.yml')
     trainer = get_trainer(config, wandb_config)
-    lightning = get_lightning(config, wandb_config)
+    lightning = get_lightning(config, wandb_config, checkpoint='fluent-durian-221-efgls6rt.ckpt')
     trainer.fit(model=lightning)
     wandb.finish()
 
@@ -61,7 +62,7 @@ def get_trainer(config, wandb_config):
     return trainer
 
 
-def get_lightning(config, wandb_config):
+def get_lightning(config, wandb_config, checkpoint=None):
     train_volumes, val_volumes = md.get_training_volumes(config, wandb_config)
     processor = utils.get_processor(config, wandb_config)
     model = ml.get_model(wandb_config)
@@ -75,7 +76,11 @@ def get_lightning(config, wandb_config):
         'val_volumes': val_volumes
     }
 
-    lightning = ml.SegSubLightning(args)
+    if checkpoint is None:
+        lightning = ml.SegSubLightning(args)
+    else:
+        path_checkpoint = os.path.join(config['path']['models']['root'], checkpoint)
+        lightning = segment_anything_ml.SegSubLightning.load_from_checkpoint(path_checkpoint, args=args)
 
     return lightning
 
