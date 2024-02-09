06980df720542bed6cdd597c7532cc1d0ca0796f
Louis REBERGA <louis.reberga@gmail.com>
2024-02-09 07:54:57 +0100

    label to border

--- src/models/losses.py
+++ src/models/losses.py
@@ -11,6 +11,7 @@ class CrossEntropyLoss(nn.Module):
 
     def forward(self, input, target):
         if self.num_labels == 1:
+            print(input.dtype, target.dtype, self.class_weights.dtype)
             cross_entropy = tF.binary_cross_entropy_with_logits(input, target.float(), pos_weight=self.class_weights)
         elif self.num_labels > 1:
             cross_entropy = tF.cross_entropy(input, target, weight=self.class_weights)
